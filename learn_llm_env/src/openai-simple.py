362f3c4337dd4132134b88f58ad08966b015658e
systemnerd <nithin_sastry@outlook.com>
2025-06-17 16:05:07 -0700

    basic API call

---
+++ learn_llm_env/src/openai-simple.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.chat.completions.create(model="gpt-4o-mini",
+                                          messages=[
+                                              {"role": "system", "content": "You are a helpful assistant."},
+                                              {"role": "user", "content": "What is the purpose of life?"},
+                                          ])
+
+print(response.choices[0].message.content)
