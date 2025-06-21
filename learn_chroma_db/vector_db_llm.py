397547bc36c5a986968184601ae01299bbbf936c
systemnerd <nithin_sastry@outlook.com>
2025-06-21 16:54:34 -0700

    covertign question into embedding

--- learn_chroma_db/vector_db_llm.py
+++ learn_chroma_db/vector_db_llm.py
@@ -22,58 +22,69 @@ collection = chroma_client.get_or_create_collection(
     embedding_function=openai_ef
 )
 
-def load_documents_from_directory(directory_name:str):
-    print("===Loading documents from directory===")
-    documents = []
-    for filename in os.listdir(directory_name):
-        if filename.endswith(".txt"):
-            with open(os.path.join(directory_name, filename)) as file:
-                documents.append({
-                    "id": filename,
-                    "text": file.read()
-                })
-    return documents
+# def load_documents_from_directory(directory_name:str):
+#     print("===Loading documents from directory===")
+#     documents = []
+#     for filename in os.listdir(directory_name):
+#         if filename.endswith(".txt"):
+#             with open(os.path.join(directory_name, filename)) as file:
+#                 documents.append({
+#                     "id": filename,
+#                     "text": file.read()
+#                 })
+#     return documents
 
-def split_text(text, chunk_size=1000, chunk_overlap=20):
-    chunks = []
-    start = 0
-    while start < len(text):
-        end = start + chunk_size
-        chunks.append(text[start:end])
-        start = end - chunk_overlap
-    return chunks
+# def split_text(text, chunk_size=1000, chunk_overlap=20):
+#     chunks = []
+#     start = 0
+#     while start < len(text):
+#         end = start + chunk_size
+#         chunks.append(text[start:end])
+#         start = end - chunk_overlap
+#     return chunks
 
-directory_path = "./data/new_articles"
-documents = load_documents_from_directory(directory_path)
+# directory_path = "./data/new_articles"
+# documents = load_documents_from_directory(directory_path)
 
-print(f"Loaded {len(documents)} documents")
+# print(f"Loaded {len(documents)} documents")
 
-# split docs into chunks
-chunked_documents = []
-for doc in documents:
-    chunks = split_text(doc["text"])
-    print("===splitting docs into chunks===")
-    for idx, chunk in enumerate(chunks):
-        chunked_documents.append({
-            "id": f"{doc['id']}_chunk{idx+1}",
-            "text": chunk
-        })
+# # split docs into chunks
+# chunked_documents = []
+# for doc in documents:
+#     chunks = split_text(doc["text"])
+#     print("===splitting docs into chunks===")
+#     for idx, chunk in enumerate(chunks):
+#         chunked_documents.append({
+#             "id": f"{doc['id']}_chunk{idx+1}",
+#             "text": chunk
+#         })
 
-def get_openai_embedding(text):
-    response = client.embeddings.create(input=text, model="text-embedding-3-small")
-    embedding = response.data[0].embedding
-    print("===Generating embedding===")
-    return embedding
+# def get_openai_embedding(text):
+#     response = client.embeddings.create(input=text, model="text-embedding-3-small")
+#     embedding = response.data[0].embedding
+#     print("===Generating embedding===")
+#     return embedding
 
-for doc in chunked_documents:
-    print("=== Generating embeddings.. ===")
-    doc["embedding"] = get_openai_embedding(doc["text"])
+# for doc in chunked_documents:
+#     print("=== Generating embeddings.. ===")
+#     doc["embedding"] = get_openai_embedding(doc["text"])
 
-for doc in chunked_documents:
-    print("=== Inserting chunks into db ===")
-    collection.upsert(
-        ids=[doc["id"]],
-        documents=[doc["text"]],
-        embeddings=[doc["embedding"]]
-    )
+# for doc in chunked_documents:
+#     print("=== Inserting chunks into db ===")
+#     collection.upsert(
+#         ids=[doc["id"]],
+#         documents=[doc["text"]],
+#         embeddings=[doc["embedding"]]
+#     )
+
+def query_documents(question, n_results=2):
+    results = collection.query(query_texts=question, n_results=n_results)
+
+    relevant_chunks = [doc for sublist in results["documents"] for doc in sublist] 
+    return relevant_chunks
+
+question = "tell me about databricks acquiring AI"
+relevant_chunks = query_documents(question)
+
+print(relevant_chunks)
 
