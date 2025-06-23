8ebf1e4e316a9a1247f93f60ad5385bb46fcdbd8
systemnerd <nithin_sastry@outlook.com>
2025-06-23 12:46:49 -0700

    simple context implementation

--- learn_llamaidx/app.py
+++ learn_llamaidx/app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from llama_index.core.agent.workflow import FunctionAgent
 from llama_index.llms.openai import OpenAI
+from llama_index.core.workflow import Context
 
 load_dotenv()
 
@@ -17,7 +18,11 @@ agent = FunctionAgent(
 )
 
 async def main():
-    response = await agent.run("whats 23.54 * 423.54?")
+    # response = await agent.run("whats 23.54 * 423.54?")
+    # print(str(response))
+    ctx = Context(agent)
+    response = await agent.run("My name is Nithin", ctx=ctx)
+    response = await agent.run("whats my name", ctx=ctx)
     print(str(response))
 
 if __name__=="__main__":
