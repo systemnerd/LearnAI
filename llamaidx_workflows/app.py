4bb7e82e53c8cf99ae476d5a6498817aadd1f8ee
systemnerd <nithin_sastry@outlook.com>
2025-06-23 20:39:22 -0700

    custom workflow.

--- llamaidx_workflows/app.py
+++ llamaidx_workflows/app.py
@@ -4,19 +4,36 @@ from llama_index.core.workflow import (
     StartEvent,
     StopEvent,
     Workflow,
-    step
+    step,
+    Event
 )
 from llama_index.utils.workflow import draw_all_possible_flows
 
+class FirstEvent(Event):
+    first_output:str
+
+class SecondEvent(Event):
+    second_output:str
+
 class SampleWorkFlow(Workflow):
     @step
-    async def step_one(self, ev:StartEvent) -> StopEvent:
-        # some custom processing
-        return StopEvent(result="Simple result saying Hello world!")
+    async def step_one(self, ev:StartEvent) -> FirstEvent:
+        print(ev.first_input)
+        return FirstEvent(first_output="step_one is done")
+    
+    @step
+    async def step_two(self, ev: FirstEvent) -> SecondEvent:
+        print(ev.first_output)
+        return SecondEvent(second_output="step_two is done")
+    
+    @step
+    async def step_three(self, ev: SecondEvent) -> StopEvent:
+        print(ev.second_output)
+        return StopEvent(result="Workflow is done")
 
 async def main():
     w = SampleWorkFlow(timeout=10,verbose=True)
-    results = await w.run()
+    results = await w.run(first_input="Start the workflow.")
     print(results)
 
 if __name__=="__main__":
