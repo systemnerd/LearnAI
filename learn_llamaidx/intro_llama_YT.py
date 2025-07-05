3476ba3c69e71563ef0b8b17ceed362a61b97ac4
systemnerd <nithin_sastry@outlook.com>
2025-07-04 19:05:46 -0700

    llamaindex basics

---
+++ learn_llamaidx/intro_llama_YT.py
@@ -0,0 +1,26 @@
+import os
+from llama_index.core import (
+                                VectorStoreIndex, 
+                                SimpleDirectoryReader, 
+                                StorageContext,
+                                load_index_from_storage
+                             )
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("./data/constitution").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
+
+response = query_engine.query("explain the third ammendment")
+print(response)
